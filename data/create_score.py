3cd475f27844a170078ef31d99eec4e59e6a98e8
cslavenc <slavenc@hotmail.ch>
2020-11-13 23:13:47 +0100

    Add initial create score file

---
+++ data/create_score.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 23:11:32 2020
+
+@author: slaven
+"""
+
+# generate a score
+import numpy as np
+import pandas as pd
+
+diversity = pd.read_csv()

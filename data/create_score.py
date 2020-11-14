f04a491f1f893ab054c0be503aa3d64e5515ac75
Luc-AI <heer.luca@gmail.com>
2020-11-14 08:50:23 +0100

    Add starter code for creating scores

--- data/create_score.py
+++ data/create_score.py
@@ -2,11 +2,31 @@
 """
 Created on Fri Nov 13 23:11:32 2020
 
-@author: slaven
+@author: slaven, luca
 """
 
 # generate a score
 import numpy as np
 import pandas as pd
 
-diversity = pd.read_csv()
+import sys
+print(sys.version)
+
+# import data
+diversity = pd.read_csv('mock_diversity_data.csv')
+
+# extract data 
+diversityData = diversity.values
+diversityHeader = pd.read_csv('mock_diversity_data.csv', nrows=1).columns
+
+# allocate data space
+z,s = np.shape(diversity)
+
+# calculate score
+
+
+# create new column
+
+
+# export data
+

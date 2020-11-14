ebf26a94d9feeb399bddca574df031ba20fea2c5
cslavenc <slavenc@hotmail.ch>
2020-11-14 08:48:49 +0100

    Add gatsby project boilerplate

---
+++ social-sustainability/src/components/layout.js
@@ -0,0 +1,28 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header />
+        <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout

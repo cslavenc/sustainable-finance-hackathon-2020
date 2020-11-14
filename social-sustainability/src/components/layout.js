153721da8ef185a19d873db9dea5373a74d98de1
elsiga <elsiga@student.ethz.ch>
2020-11-14 11:46:16 +0100

    we have the PHP sponsorshipgit add .

--- social-sustainability/src/components/layout.js
+++ social-sustainability/src/components/layout.js
@@ -1,26 +1,17 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
+import Navigation from "./navigation"
+
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <Header />
+      <Navigation/>
         <main>{children}</main>
       <Footer />
     </>
